1c00ec1adb53a3511922ef1d104dd8086afb740c
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-03-14 15:29:17 +0100

    :heavy_plus_sign: add helmet and cors management

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,27 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        /^http:\/\/localhost(:\d+)?$/.test(origin) ||
+        /^https?:\/\/([a-z0-9-]+\.)*studio-klypi\.com$/.test(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
