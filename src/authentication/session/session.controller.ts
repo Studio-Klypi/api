9417c38cea5af24b28163ea7978ef3ef54dd21e1
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-03-26 03:45:47 +0100

    :rocket: add me endpoint

--- src/authentication/session/session.controller.ts
+++ src/authentication/session/session.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Post, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import type { Session } from '@prisma/client';
 import { SessionService } from './session.service';
 import { CreateSessionDto } from './dto/create-session.dto';
@@ -6,6 +14,8 @@ import { type Response } from 'express';
 import { AuthGuard } from '../../common/guards/auth.guard';
 import { IsMeGuard } from '../../common/guards/is-me.guard';
 import { GetSession } from '../../common/decorators/get-session.decorator';
+import { GetUser } from '../../common/decorators/get-user.decorator';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Controller('auth')
 export class SessionController {
@@ -26,5 +36,11 @@ export class SessionController {
     @Res({ passthrough: true }) res: Response,
   ) {
     return this.service.kill(session.sid, session.userId, res);
+  }
+
+  @Get('me')
+  @UseGuards(AuthGuard)
+  recover(@GetUser() user: UserEntity) {
+    return user;
   }
 }
